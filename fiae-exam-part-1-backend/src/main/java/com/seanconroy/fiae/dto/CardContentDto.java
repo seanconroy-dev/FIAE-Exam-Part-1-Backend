e2c7a91e7774ef97a42ea4f1db7174af4fbc4caa
Sean Conroy <123978695+seanconroy-dev@users.noreply.github.com>
2026-04-20 10:25:39 +0200

    Add CardContentDto class for representing card content data

---
+++ fiae-exam-part-1-backend/src/main/java/com/seanconroy/fiae/dto/CardContentDto.java
@@ -0,0 +1,12 @@
+package com.seanconroy.fiae.dto;
+
+import java.util.List;
+
+public class CardContentDto {
+    public String type;
+    public String question;
+    public String image;
+    public String answer;
+    public String answerImage;
+    public List<String> examples;
+}
