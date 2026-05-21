e47cd885584781668661c7230dd779510c66d911
Sean Conroy <123978695+seanconroy-dev@users.noreply.github.com>
2026-05-21 13:01:29 +0200

    feat: add AuthContext class for managing the current user session

---
+++ fiae-exam-part-1-backend/src/main/java/com/seanconroy/fiae/service/AuthContext.java
@@ -0,0 +1,18 @@
+package com.seanconroy.fiae.service;
+
+import com.seanconroy.fiae.entity.WhitelistUser;
+import jakarta.enterprise.context.RequestScoped;
+
+@RequestScoped
+public class AuthContext {
+
+    private WhitelistUser currentUser;
+
+    public WhitelistUser getCurrentUser(){
+        return currentUser;
+    }
+
+    public void setCurrentUser(WhitelistUser currenUser){
+        this.currentUser = currenUser;
+    }
+}
