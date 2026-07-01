9f12dc9658682b5eb118485c74bc68239bb93917
Sean Conroy <123978695+seanconroy-dev@users.noreply.github.com>
2026-07-01 13:34:19 +0200

    feat: create UpdateProgressRequestDto class for handling progress updates

---
+++ fiae-exam-part-1-backend/src/main/java/com/seanconroy/fiae/dto/UpdateProgressRequestDto.java
@@ -0,0 +1,19 @@
+package com.seanconroy.fiae.dto;
+
+public class UpdateProgressRequestDto {
+
+    public boolean wasCorrect;
+
+    public UpdateProgressRequestDto() {
+
+    }
+
+    public boolean getWasCorrect() {
+        return wasCorrect;
+    }
+
+    public void setWasCorrect(boolean wasCorrect) {
+        this.wasCorrect = wasCorrect;
+    }
+
+}
